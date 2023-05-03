8aea4f4b86547966c1b0e0c7985166ac53bf3f9f
aldermoonwr18 <aldermoonwr@gmail.com>
2023-05-03 18:34:05 -0400

    Saving and Main Menu
    Added some scripts to get a save function working and working on remaking the main menu.

---
+++ Assets/Scripts/Character/Player.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Temp variables for player script.
+    public int level = 0;
+    public int health = 100;
+}
