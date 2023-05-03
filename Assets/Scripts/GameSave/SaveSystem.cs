6c05678a0a8b46cc91a6ed2ecfa96599520a5f45
aldermoonwr18 <aldermoonwr@gmail.com>
2023-05-03 19:20:26 -0400

    Main Menu Script
    Made main menu script

--- Assets/Scripts/GameSave/SaveSystem.cs
+++ Assets/Scripts/GameSave/SaveSystem.cs
@@ -4,5 +4,8 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 public static class SaveSystem
 {
-    
+    public static void SavePlayer (Player player)
+    {
+
+    }
 }
---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // First UI that is shown first.
+    public GameObject title;
+    public GameObject play;
+    public GameObject settings;
+    public GameObject exit;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (title == null) title = GameObject.Find("Title");
+        if (play == null) play = GameObject.Find("Play");
+        if (settings == null) settings = GameObject.Find("Settings");
+        if (exit == null) exit = GameObject.Find("Exit");
+
+        // Activating title, play, exit, and settings buttons.
+        title.SetActive(true);
+        play.SetActive(true);
+        settings.SetActive(true);
+        exit.SetActive(true);
+    }
+}
