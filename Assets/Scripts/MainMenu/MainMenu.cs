8c6b9256987a6b6dfb6db13bd44663994cabcef7
aldermoonwr18 <aldermoonwr@gmail.com>
2023-05-03 22:15:17 -0400

    Main Menu
    Added to the main menu script and added UI for the 3 different save files for the game

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    // First UI that is shown first.
+    // GameObjects for main UI
     public GameObject title;
     public GameObject play;
     public GameObject settings;
     public GameObject exit;
 
+    // GameObjects for Game Saves
+    public GameObject saveScreen;
+    public GameObject gameOneBar;
+    public GameObject gameTwoBar;
+    public GameObject gameThreeBar;
+    public GameObject gameOneButton;
+    public GameObject gameTwoButton;
+    public GameObject gameThreeButton;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +27,38 @@ public class MainMenu : MonoBehaviour
         if (settings == null) settings = GameObject.Find("Settings");
         if (exit == null) exit = GameObject.Find("Exit");
 
-        // Activating title, play, exit, and settings buttons.
+        if (saveScreen == null) saveScreen = GameObject.Find("Save Screen");
+        if (gameOneBar == null) gameOneBar = GameObject.Find("GameOne");
+        if (gameTwoBar == null) gameTwoBar = GameObject.Find("GameTwo");
+        if (gameThreeBar == null) gameThreeBar = GameObject.Find("GameThree");
+        if (gameOneButton == null) gameOneButton = GameObject.Find("GameOnePlay");
+        if (gameTwoButton == null) gameTwoButton = GameObject.Find("GameTwoPlay");
+        if (gameThreeButton == null) gameThreeButton = GameObject.Find("GameThreePlay");
+
+        // Activating main UI game objects.
         title.SetActive(true);
         play.SetActive(true);
         settings.SetActive(true);
         exit.SetActive(true);
-        //
+
+        // Deactivating other UI.
+        saveScreen.SetActive(false);
+        gameOneBar.SetActive(false);
+        gameTwoBar.SetActive(false);
+        gameThreeBar.SetActive(false);
+        gameOneButton.SetActive(false);
+        gameTwoButton.SetActive(false);
+        gameThreeButton.SetActive(false);
+    }
+
+    public void GameSaves()
+    {
+        saveScreen.SetActive(true);
+        gameOneBar.SetActive(true);
+        gameTwoBar.SetActive(true);
+        gameThreeBar.SetActive(true);
+        gameOneButton.SetActive(true);
+        gameTwoButton.SetActive(true);
+        gameThreeButton.SetActive(true);
     }
 }
