28567e55f5ffcd4854b5e3d7b5b170313716ffef
aldermoonwr18 <aldermoonwr@gmail.com>
2023-05-05 01:32:00 -0400

    Main Menu UI & Saving
    Started work on creating save files, finished main menu UI.

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class MainMenu : MonoBehaviour
     public GameObject play;
     public GameObject settings;
     public GameObject exit;
+    public GameObject back;
 
     // GameObjects for Game Saves
     public GameObject saveScreen;
@@ -19,6 +21,13 @@ public class MainMenu : MonoBehaviour
     public GameObject gameTwoButton;
     public GameObject gameThreeButton;
 
+    // GameObjects for Settings Screen
+    public GameObject settingsScreen;
+
+    // Bools for menus
+    public bool saveMenu;
+    public bool settingsMenu;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +35,7 @@ public class MainMenu : MonoBehaviour
         if (play == null) play = GameObject.Find("Play");
         if (settings == null) settings = GameObject.Find("Settings");
         if (exit == null) exit = GameObject.Find("Exit");
+        if (back == null) back = GameObject.Find("Back");
 
         if (saveScreen == null) saveScreen = GameObject.Find("Save Screen");
         if (gameOneBar == null) gameOneBar = GameObject.Find("GameOne");
@@ -35,6 +45,8 @@ public class MainMenu : MonoBehaviour
         if (gameTwoButton == null) gameTwoButton = GameObject.Find("GameTwoPlay");
         if (gameThreeButton == null) gameThreeButton = GameObject.Find("GameThreePlay");
 
+        if (settingsScreen == null) settingsScreen = GameObject.Find("Settings Screen");
+
         // Activating main UI game objects.
         title.SetActive(true);
         play.SetActive(true);
@@ -43,22 +55,63 @@ public class MainMenu : MonoBehaviour
 
         // Deactivating other UI.
         saveScreen.SetActive(false);
-        gameOneBar.SetActive(false);
-        gameTwoBar.SetActive(false);
-        gameThreeBar.SetActive(false);
-        gameOneButton.SetActive(false);
-        gameTwoButton.SetActive(false);
-        gameThreeButton.SetActive(false);
+        settingsScreen.SetActive(false);
+        back.SetActive(false);
+
+        // Sets bools for menus to false
+        saveMenu = false;
+        settingsMenu = false;
+
+        // Set unused play buttons to inactive here.
     }
 
+    // Opens up the menu with the 3 game saves.
     public void GameSaves()
     {
         saveScreen.SetActive(true);
-        gameOneBar.SetActive(true);
-        gameTwoBar.SetActive(true);
-        gameThreeBar.SetActive(true);
-        gameOneButton.SetActive(true);
-        gameTwoButton.SetActive(true);
-        gameThreeButton.SetActive(true);
+        back.SetActive(true);
+
+        saveMenu = true;
+
+        // Making play button unable to be clicked in this mode.
+        play.GetComponent<Button>().enabled = false;
+    }
+
+    // Opens the settings menu.
+    public void Settings()
+    {
+        settingsScreen.SetActive(true);
+        back.SetActive(true);
+
+        settingsMenu = true;
+
+        // Making play button unable to be clicked in this mode.
+        play.GetComponent<Button>().enabled = false;
+        settings.GetComponent<Button>().enabled = false;
+    }
+
+    // Closes a menu that is open.
+    public void CloseMenu()
+    {
+        back.SetActive(false);
+        play.GetComponent<Button>().enabled = true;
+        settings.GetComponent<Button>().enabled = true;
+        if (saveMenu) saveScreen.SetActive(false);
+        if (settingsMenu) settingsScreen.SetActive(false);
+    }
+
+    // Exits the game
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void PlayGame()
+    {
+        // Check if game is created based on button name.
+
+        // If game not created create new game, call NewGame().
+
+        // If game HAS been created, open it.
     }
 }
